a2fed9c5ea2192da825c074137b062c8d9192513
adrianGS25 <chgarciasa@uide.edu.ec>
2026-03-20 18:02:41 +0000

    libs and key generation

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,4 @@
 uname -a # Da info del ordenador
 which gpg # ubicacion del binario del ordenador
 gpg --version # me da la version e informacion adicional
+gpg --full-generate-key # con esto genero la llave publica y privada
