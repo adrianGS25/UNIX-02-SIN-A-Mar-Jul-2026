4836e931a72a7062c38dcb8522c4024ca77e189e
adrianGS25 <chgarciasa@uide.edu.ec>
2026-03-26 12:43:32 +0000

    PASSWORD

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,5 @@ uname -a # Da info del ordenador
 which gpg # ubicacion del binario del ordenador
 gpg --version # me da la version e informacion adicional
 gpg --full-generate-key # con esto genero la llave publica y privada
+# 7EC2C36E917D6D0B
+ gpg --armor --export-secret-keys 7EC2C36E917D6D0B # Exporta clave privada del ID
