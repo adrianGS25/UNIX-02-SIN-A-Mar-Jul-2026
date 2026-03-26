58bcd205946fa857bccd589100aef96badc8d573
adrianGS25 <chgarciasa@uide.edu.ec>
2026-03-26 13:05:47 +0000

    compallave

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -5,3 +5,4 @@ gpg --full-generate-key # con esto genero la llave publica y privada
 # 7EC2C36E917D6D0B
  gpg --armor --export-secret-keys 7EC2C36E917D6D0B # Exporta clave privada del ID
  gpg --armor --export adriangarciasnchez@gmail.com  > mi_llave_publica.asc # Exporta mi llave publica
+ gpg --import micompa.asc  llave-publica.asc # Importa la llave de mi compa
