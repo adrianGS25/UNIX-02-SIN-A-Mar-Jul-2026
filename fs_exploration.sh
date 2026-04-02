3b63be01b6dbcfeb0cd4d0f799637a87b9376d88
adrianGS25 <chgarciasa@uide.edu.ec>
2026-04-02 12:46:44 +0000

    sudo

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -1,2 +1,5 @@
 cd / # change directory
 ls -F # lists files
+sudo apt update # Updates the package list from repositories
+sudo apt upgrade # Upgrades all installed packages to their latest versions
+sudo apt install parted # install parted 
