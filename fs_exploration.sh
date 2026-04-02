c23826860e5d01b7ef3a359b99c7693f83d06cfe
adrianGS25 <chgarciasa@uide.edu.ec>
2026-04-02 12:59:14 +0000

    parted

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -3,3 +3,4 @@ ls -F # lists files
 sudo apt update # Updates the package list from repositories
 sudo apt upgrade # Upgrades all installed packages to their latest versions
 sudo apt install parted # install parted 
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e  # That command combines several instructions to display detailed information about your disks and partitions
