4473e330e5e1f3ce9d37724be64fca9c923652e8
adrianGS25 <chgarciasa@uide.edu.ec>
2026-04-16 13:08:58 +0000

    helps in the terminal

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -6,3 +6,9 @@ ls -l -a -h # It is the "complete package" for inspecting a directory.
 ls -lah # It is the "complete package" for inspecting a directory.
 mkdir -- -rf # It creates a directory literally named -rf
 rmdir -- -rf # It removes the directory literally named -rf.
+-- depth # Create a shallow clone with a history truncated to the specified number of commits. Implies
+        # --single-branch unless --no-single-branch is given to fetch the histories near the tips of all branches.
+          # If you want to clone submodules shallowly, also pass --shallow-submodules.
+# We use /(the comand you want to search) to see what it means
+# n to go down
+# N to go up 
