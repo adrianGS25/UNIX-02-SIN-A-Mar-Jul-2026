1e22a8edc69c4a50f31f9ab2cc43ebfe8ced4efd
adrianGS25 <chgarciasa@uide.edu.ec>
2026-04-16 12:51:51 +0000

    list files

---
+++ boot_exploration.sh
@@ -0,0 +1,6 @@
+ls -a # transform the simple list into a detailed table:
+ls --all # transform the simple list into a detailed table:
+ls -l # List the contents of a directory in long format
+ls -h # It is used to make file sizes easy for a human to read.
+ls -l -a -h # It is the "complete package" for inspecting a directory.
+ls -lah # It is the "complete package" for inspecting a directory.
