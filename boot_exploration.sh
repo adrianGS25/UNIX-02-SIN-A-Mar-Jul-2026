886bea86f5902b74aecf5ab1969b56e6627fec81
adrianGS25 <chgarciasa@uide.edu.ec>
2026-04-23 12:33:40 +0000

    add chao

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -19,3 +19,9 @@ chmod u+rw.go-rwx privado # dueño lee/escribe y nadie mas puede hacerlo
 sudo echo "hola" > /etc/archivo_protegido # It doesn´t work correctly because sudo doesn´t apply to redirection (>)
 sudo echo "hola" | sudo tee /etc/archivo_protegido > /dev/null # write to a protected file using sudo correctly
 sudo echo "hola" | sudo tee /etc/archivo_protegido # write "hola" on a protected achive using admin permission
+
+# Clase 23/4/2026
+sudo sh -c 'echo "chao" >> /etc/archivo_protegido' # Add the word "chao" to the end of a protected system file using administrator privileges
+cat /etc/archivo_protegido # Display the contents of the protected file directly on your screen.
+sudo -i # Abre una sesión interactiva como superusuario (root), convirtiendo tu terminal en una consola de administrador total.
+exit # Close the current terminal session or exit administrator mode to return to your normal user account.
