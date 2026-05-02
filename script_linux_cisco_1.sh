4b6a33496c9e379d72c8343a180b633b18fac2fd
adrianGS25 <chgarciasa@uide.edu.ec>
2026-05-02 17:17:15 +0000

    change directory

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -5,3 +5,10 @@ ls -l # Provides a long list format with more file information
 ls -r # Lists files in reverse alphabetical order
 ls -lr # Combined options for a long list in reverse order
 pwd # Prints the current working directory path
+cd Documents # Navigates to the Documents directory using a relative path
+cd / # Navigates to the root directory of the system
+cd /home/sysadmin # Navigates to the home directory using an absolute path
+cd School/Art # Navigates to the Art directory using a multi-level relative path
+cd .. # Moves up one level to the parent directory
+cd . # Represents the current directory (no movement)
+cd ~ # Returns to the current user's home directory
