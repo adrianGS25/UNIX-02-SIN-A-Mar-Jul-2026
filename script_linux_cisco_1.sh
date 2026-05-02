7cc9d3ba455ea798c560cd0b3319b99368e9cde5
adrianGS25 <chgarciasa@uide.edu.ec>
2026-05-02 17:12:15 +0000

    arguments

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,3 +1,3 @@
 ls # The 'ls' command lists information about files in the current directory.
 LS # LS is incorrect and will generate an error message because Linux is case sensitive
-
+ls Documents # Lists the contents of the Documents directory
