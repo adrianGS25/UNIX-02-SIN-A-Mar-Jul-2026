7f8da98cc5695e8ffc9d0f3bbfa62f3beae45980
adrianGS25 <chgarciasa@uide.edu.ec>
2026-05-02 17:14:05 +0000

    options

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,3 +1,6 @@
 ls # The 'ls' command lists information about files in the current directory.
 LS # LS is incorrect and will generate an error message because Linux is case sensitive
 ls Documents # Lists the contents of the Documents directory
+ls -l # Provides a long list format with more file information
+ls -r # Lists files in reverse alphabetical order
+ls -lr # Combined options for a long list in reverse order
