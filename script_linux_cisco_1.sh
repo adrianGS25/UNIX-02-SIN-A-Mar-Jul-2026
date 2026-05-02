6497b00a9dd3226a139375ec6cf86612a689ef94
adrianGS25 <chgarciasa@uide.edu.ec>
2026-05-02 17:15:12 +0000

    Print the working directory

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -4,3 +4,4 @@ ls Documents # Lists the contents of the Documents directory
 ls -l # Provides a long list format with more file information
 ls -r # Lists files in reverse alphabetical order
 ls -lr # Combined options for a long list in reverse order
+pwd # Prints the current working directory path
