8096d15960750225ddea7e77dd650123a6a247d6
adrianGS25 <chgarciasa@uide.edu.ec>
2026-05-28 12:57:45 +0000

    create file

--- securitydos.zsh
+++ securitydos.zsh
@@ -3,3 +3,6 @@ id -gn # only the name of the principal group
 ls -la # to see what is the main group of the user
 touch ~/test_grupo_heredado.txt # creates a file and see what group inherits
 ls -la ~/test_grupo_heredado.txt # main group of the user
+echo "Grupo actual: $(id -gn)" # se the actual group
+touch ~/antes_de_newgrp.txt # create a new file before newgrp
+ls -ls ~/antes_de_newgrp.txt # create a new file before newgrp
