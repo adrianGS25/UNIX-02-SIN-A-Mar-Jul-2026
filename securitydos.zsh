9da2a173d89b8d009c8b8fd5e9bc383663c98f87
adrianGS25 <chgarciasa@uide.edu.ec>
2026-05-28 12:51:20 +0000

    file and group

---
+++ securitydos.zsh
@@ -0,0 +1,5 @@
+id # see the main group
+id -gn # only the name of the principal group
+ls -la # to see what is the main group of the user
+touch ~/test_grupo_heredado.txt # creates a file and see what group inherits
+ls -la ~/test_grupo_heredado.txt # main group of the user
