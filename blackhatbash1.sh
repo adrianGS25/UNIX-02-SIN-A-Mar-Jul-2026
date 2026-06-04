5149215c62238278ba64702d868b3808e3e5d865
adrianGS25 <chgarciasa@uide.edu.ec>
2026-06-04 13:18:36 +0000

    blackhatbash1

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-set -x
-bash --version
-env
-echo ${SHELL}
-echo ${RANDOM}
-echo ${UID}
-echo ${EUID}
-echo ${OSTYPE}
-ps -ef
-bash -r blackhatbash1.sh
-bash -n blackhatbash1.sh
-bash -x blackhatbash1.sh
-set +x
+set -x # Enables debugging mode (prints each command before executing it).
+bash --version # Displays the current version and license information of the installed Bash shell.
+env # Lists all active environment variables for the current session.
+echo ${SHELL} # Prints the path to the current user's default shell (e.g., /bin/bash).
+echo ${RANDOM} # Generates and prints a random integer between 0 and 32767.
+echo ${UID} # Prints the real User ID of the current user (0 for root).
+echo ${EUID} # Prints the Effective User ID (used by the operating system to check permissions)
+echo ${OSTYPE} # Prints the type of operating system Bash is running on (e.g., linux-gnu).
+ps -ef # Displays a snapshot of every active process running on the system in full format.
+bash -r blackhatbash1.sh # Runs the script inside a "Restricted Shell" environment for security testing.
+bash -n blackhatbash1.sh # Performs a syntax check on the script without actually running any code
+bash -x blackhatbash1.sh # Executes the script with debugging enabled specifically for this run.
+set +x # Disables the debugging mode
