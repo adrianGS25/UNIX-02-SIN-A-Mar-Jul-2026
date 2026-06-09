677d1b6d9354474c6ab3f8a17492be50772ba03d
adrianGS25 <chgarciasa@uide.edu.ec>
2026-06-09 16:54:15 +0000

    input prompting

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -12,3 +12,10 @@ echo "The total number of arguments is: $#"
 for arg in "$@"; do
 echo "${arg}"
 done
+
+# Takes input from the user and assigns it to variables 
+echo "What is your first name?" 
+read -r firstname 
+echo "What is your last name?" 
+read -r lastname 
+echo "Your first name is ${firstname} and your last name is ${lastname}"
