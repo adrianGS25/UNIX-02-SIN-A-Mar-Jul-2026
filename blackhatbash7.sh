9cf21498c5bd21e0d0fbe522f4b0e899d0868457
adrianGS25 <chgarciasa@uide.edu.ec>
2026-06-18 12:41:43 +0000

    TTY

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -2,3 +2,6 @@
  grep "35.237.4.214" log.txt
  grep "35.237.4.214\|13.66.139.0" log.txt
  grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+ ps | grep TTY # Lists all processes and filters for those that have "TTY" in their output, which indicates terminal sessions.
+ps | grep -i tty # Lists all processes and filters for those that have "tty" in their output, ignoring case sensitivity.
+
