f7608b549b506052c693924d5982a6c7a5fe0cd8
adrianGS25 <chgarciasa@uide.edu.ec>
2026-06-18 12:44:05 +0000

    -o-v

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -5,3 +5,5 @@
  ps | grep TTY # Lists all processes and filters for those that have "TTY" in their output, which indicates terminal sessions.
 ps | grep -i tty # Lists all processes and filters for those that have "tty" in their output, ignoring case sensitivity.
 
+grep -v "35.237.4.214" log.txt # Displays all lines in log.txt that do not contain "35.237.4.214"
+grep -o "35.237.4.214" log.txt # Displays only the occurrences of "35.237.4.214" in log.txt
